90c4df8af5b3e9e8dd800462a1d52db83644912e
EndyKaufman <admin@site15.ru>
2022-07-14 23:49:23 +0400

    chore: update index file

--- libs/nestjs-custom-injector/src/index.ts
+++ libs/nestjs-custom-injector/src/index.ts
@@ -4,3 +4,4 @@ export * from './lib/custom-injector.module';
 export * from './lib/custom-injector.service';
 export * from './lib/custom-injector.storage';
 export * from './lib/custom-injector.types';
+export * from './lib/custom-injector.utils';
